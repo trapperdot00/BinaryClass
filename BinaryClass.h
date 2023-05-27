92f60eee560a4f5ba42173e40286ef4079612af8
trapperdot00 <126814871+trapperdot00@users.noreply.github.com>
2023-05-27 21:42:03 +0200

    default constructor fix, styling

--- BinaryClass.h
+++ BinaryClass.h
@@ -2,6 +2,7 @@
 #include <string>
 
 class Binary;
+
 const std::string decToBin(const long long unsigned&);
 const unsigned binToDec(const std::string&);
 const unsigned binToDec(const Binary&);
@@ -9,11 +10,12 @@ const int powerOfNum(const int&, const int&);
 
 class Binary {
     friend const unsigned binToDec(const Binary&);
+
 public:
-    Binary() = default;
-    Binary(const long long unsigned& decVal);
-    Binary(const std::string&);
-    Binary(const char*);
+    explicit Binary(const long long unsigned& decVal);
+    explicit Binary(const std::string&);
+    explicit Binary(const char*);
+    Binary() : binaryValue("0") {}
 
     std::ostream& printBin(std::ostream&) const;
     std::ostream& printDec(std::ostream&) const;
@@ -21,11 +23,12 @@ public:
     std::istream& readDec(std::istream&);
     const std::string& getValueBin() const { return binaryValue; }
     const unsigned getValueDec() const;
+
 private:
-    std::string binaryValue = "0";
     const std::string strip(const std::string&) const;
     const char* strip(const char*) const;
-    const bool isBinaryString(const std::string& s) const;
+    bool isBinaryString(const std::string&) const;
+    bool isBinaryString(const char*) const;
 
+    std::string binaryValue;
 };
-
